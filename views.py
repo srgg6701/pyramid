99bd3e53f4eee159fdd1291261094ebd0ca7f70f
srgg6701 <srgg67@gmail.com>
2014-04-22 21:54:04 +0400

    Start using variables. Bug fixed.

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/test.pt')
 def my_view(request):
-    return {'project': 'TestPyramid'}
+    return {'content': 'My project name is TestPyramid'}
 @view_config(route_name='gate', renderer='templates/test.pt')
 def my_gate(request):
-    return {'gate': 'This is the Star Gate!'}
+    return {'content': 'This is the Star Gate!'}
