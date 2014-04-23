7d0cd9b72c76a8c480b63105b817ef518670256c
srgg6701 <srgg67@gmail.com>
2014-04-23 16:16:48 +0400

    Towards to new approach.

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,4 +1,10 @@
 function mainController($scope){
     $scope.test="Angular test comes here!";
-    $scope.some_data="Do you see any data?";
+    //$scope.some_data="Do you see any data?";
+    $scope.goAngular = function(){
+        $scope.content_data = "Here are lots of workers!";
+    };
+    $scope.goJS = function(){
+        $scope.content_data = "Here is some shedule...";
+    };
 }
