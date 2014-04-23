782727d49ee32d7f6502f45786a7842469d655ad
srgg6701 <srgg67@gmail.com>
2014-04-23 17:24:24 +0400

    Refactoring, improving.

--- static/js/controller.js
+++ static/js/controller.js
@@ -3,10 +3,17 @@ function mainController($scope){
     //$scope.some_data="Do you see any data?";
     $scope.goAngular = function(){
         $scope.content_data = "Here are lots of workers!";
-        $scope.section.state = "angular";
+        $scope.section.state = "task";
     };
     $scope.goJS = function(){
         $scope.content_data = "Here is some shedule...";
-        $scope.section.state = "js";
+        $scope.section.state = "task";
     };
+    $scope.servdata=[
+        {address:'http://127.0.0.1', port:8888, ssl:true},
+        {address:'http://super.serv.com', port:3030, ssl:false},
+        {address:'http://192.168.1.9', port:443, ssl:true},
+        {address:'http://awesomeserver.org', port:80, ssl:true},
+        {address:'http://megarobust.net', port:3137, ssl:true}
+    ];
 }
