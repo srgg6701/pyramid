31aa4d4f0237f1f1cab5332369cfc43e521334f6
srgg6701 <srgg67@gmail.com>
2014-04-23 16:39:11 +0400

    Data handling prototype is set.

--- static/js/controller.js
+++ static/js/controller.js
@@ -3,8 +3,10 @@ function mainController($scope){
     //$scope.some_data="Do you see any data?";
     $scope.goAngular = function(){
         $scope.content_data = "Here are lots of workers!";
+        $scope.section.state = "angular";
     };
     $scope.goJS = function(){
         $scope.content_data = "Here is some shedule...";
+        $scope.section.state = "js";
     };
 }
--- views.py
+++ views.py
@@ -2,7 +2,4 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/metro.pt')
 def my_view(request):
-    return {'content': 'My project name is TestPyramid'}
-@view_config(route_name='gate', renderer='templates/test.pt')
-def my_gate(request):
-    return {'content': 'This is the Star Gate!'}
+    return {}
